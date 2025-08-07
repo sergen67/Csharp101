33025d95e9795a8e3c79cc548247db593b45e8bf
sergen67 <149192311+sergen67@users.noreply.github.com>
2025-08-07 09:50:56 +0300

    Add files via upload

---
+++ CSharpEgitim/01_MainSubjects/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region YazdırmaKomutları
+            //Console.WriteLine("Merhaba Dünya!");
+            //Console.Write("Selam");
+
+            //Console.WriteLine("******Menü******");
+            //Console.WriteLine();
+            //Console.WriteLine("     Çorbalar");
+            //Console.WriteLine("     Lahmacun");
+            //Console.WriteLine("     Pideler");
+            //Console.WriteLine("     Adana Kebap");
+            //Console.WriteLine("     Urfa Kebap");
+            //Console.WriteLine("     İskender Kebap");
+            //Console.WriteLine("     İçecekler");
+            #endregion
+
+            #region Değişkenler
+
+            //string name;
+            //name = "Sergen";
+            //Console.WriteLine(name);
+
+
+            //string customerName;
+            //string customerSurname ;
+            //string customerAge ;
+            //string customerPhone;
+
+            //customerName = "Sergen";
+            //customerSurname = "Yağlı";
+            //customerAge = "20";
+            //customerPhone = "555-555-55-55";
+
+            //Console.WriteLine("Müşteri Adı: " + customerName);
+            //Console.WriteLine("Müşteri Soyadı: " + customerSurname);
+            //Console.WriteLine("Müşteri Yaşı: " + customerAge);
+            //Console.WriteLine("Müşteri Telefonu: " + customerPhone);
+            //Console.WriteLine("-----------");
+
+            //customerName = "İlhan";
+            //Console.WriteLine(customerName);
+
+            #endregion
+
+            #region INTdeğişkenler
+
+            int number = 21;
+            Console.WriteLine("Sayı: " + number);
+
+            int lahmacunPrice = 200;
+            int colaPrice = 50;
+            int waterPrice = 10;
+            int pidePrice = 250;
+            int ayranPrice = 25;
+
+
+            Console.WriteLine("*******Fiyat Listesi******");
+            Console.WriteLine();
+            Console.WriteLine("Lahmacun Fiyatı: " + lahmacunPrice + " TL");
+            Console.WriteLine("kola Fiyatı: " + colaPrice + " TL");
+            Console.WriteLine("su Fiyatı: " + waterPrice + " TL");
+            Console.WriteLine("pide Fiyatı: " + pidePrice + " TL");
+            Console.WriteLine("ayran Fiyatı: " + ayranPrice + " TL");
+
+
+
+            Console.WriteLine();
+            int lahmacunCount;
+            int colaCount;
+            int waterCount;
+            int pideCount;  
+            int ayranCount; 
+            int totalPrice = 0;
+
+            lahmacunCount = 2;
+            colaCount = 1;
+            waterCount = 1;
+            pideCount = 1;
+            ayranCount = 0;
+
+            totalPrice = lahmacunPrice* lahmacunCount +
+                          colaPrice * colaCount +
+                          waterPrice * waterCount +
+                          pidePrice * pideCount +
+                          ayranPrice * ayranCount;
+
+            Console.WriteLine("Toplam Tutar: " + totalPrice + " TL");
+
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
