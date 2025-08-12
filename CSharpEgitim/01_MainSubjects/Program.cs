241d956beddc79a3e48e3e9d6d38afe886441110
sergen67 <149192311+sergen67@users.noreply.github.com>
2025-08-12 15:49:15 +0300

    Add files via upload

--- CSharpEgitim/01_MainSubjects/Program.cs
+++ CSharpEgitim/01_MainSubjects/Program.cs
@@ -55,52 +55,52 @@ namespace _01_MainSubjects
 
             #region INTdeğişkenler
 
-            int number = 21;
-            Console.WriteLine("Sayı: " + number);
+            //int number = 21;
+            //Console.WriteLine("Sayı: " + number);
 
-            int lahmacunPrice = 200;
-            int colaPrice = 50;
-            int waterPrice = 10;
-            int pidePrice = 250;
-            int ayranPrice = 25;
-
-
-            Console.WriteLine("*******Fiyat Listesi******");
-            Console.WriteLine();
-            Console.WriteLine("Lahmacun Fiyatı: " + lahmacunPrice + " TL");
-            Console.WriteLine("kola Fiyatı: " + colaPrice + " TL");
-            Console.WriteLine("su Fiyatı: " + waterPrice + " TL");
-            Console.WriteLine("pide Fiyatı: " + pidePrice + " TL");
-            Console.WriteLine("ayran Fiyatı: " + ayranPrice + " TL");
+            //int lahmacunPrice = 200;
+            //int colaPrice = 50;
+            //int waterPrice = 10;
+            //int pidePrice = 250;
+            //int ayranPrice = 25;
 
 
+            //Console.WriteLine("*******Fiyat Listesi******");
+            //Console.WriteLine();
+            //Console.WriteLine("Lahmacun Fiyatı: " + lahmacunPrice + " TL");
+            //Console.WriteLine("kola Fiyatı: " + colaPrice + " TL");
+            //Console.WriteLine("su Fiyatı: " + waterPrice + " TL");
+            //Console.WriteLine("pide Fiyatı: " + pidePrice + " TL");
+            //Console.WriteLine("ayran Fiyatı: " + ayranPrice + " TL");
 
-            Console.WriteLine();
-            int lahmacunCount;
-            int colaCount;
-            int waterCount;
-            int pideCount;  
-            int ayranCount; 
-            int totalPrice = 0;
 
-            lahmacunCount = 2;
-            colaCount = 1;
-            waterCount = 1;
-            pideCount = 1;
-            ayranCount = 0;
 
-            totalPrice = lahmacunPrice* lahmacunCount +
-                          colaPrice * colaCount +
-                          waterPrice * waterCount +
-                          pidePrice * pideCount +
-                          ayranPrice * ayranCount;
+            //Console.WriteLine();
+            //int lahmacunCount;
+            //int colaCount;
+            //int waterCount;
+            //int pideCount;  
+            //int ayranCount; 
+            //int totalPrice = 0;
 
-            Console.WriteLine("Toplam Tutar: " + totalPrice + " TL");
+            //lahmacunCount = 2;
+            //colaCount = 1;
+            //waterCount = 1;
+            //pideCount = 1;
+            //ayranCount = 0;
+
+            //totalPrice = lahmacunPrice* lahmacunCount +
+            //              colaPrice * colaCount +
+            //              waterPrice * waterCount +
+            //              pidePrice * pideCount +
+            //              ayranPrice * ayranCount;
+
+            //Console.WriteLine("Toplam Tutar: " + totalPrice + " TL");
 
 
             #endregion
 
-            Console.Read();
+          
         }
     }
 }
--- CSharpEgitim/DatabasesCrud/Program.cs
+++ CSharpEgitim/DatabasesCrud/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -16,22 +17,105 @@ namespace DatabasesCrud
             Console.WriteLine();
 
             Console.WriteLine("----------------");
-            Console.Write("Eklemek istediğiniz kategori adı:");
-            string categoryName = Console.ReadLine();
+            #region Kategori Ekleme İşlemi
+            //Console.Write("Eklemek istediğiniz kategori adı:");
+            //string categoryName = Console.ReadLine();
 
 
-            SqlConnection connection = new SqlConnection("Data Source = DESKTOP-OBIK10F;initial catalog = EgitimKampidb;integrated security = true ");
-            connection.Open();
+            //SqlConnection connection = new SqlConnection("Data Source = DESKTOP-OBIK10F;initial catalog = EgitimKampidb;integrated security = true ");
+            //connection.Open();
 
-            SqlCommand command = new SqlCommand("insert into Table_1(CategoryName) values (@p1)",connection);
-            command.Parameters.AddWithValue("@p1", categoryName);
-            command.ExecuteNonQuery();
-            connection.Close();
+            //SqlCommand command = new SqlCommand("insert into Table_1(CategoryName) values (@p1)",connection);
+            //command.Parameters.AddWithValue("@p1", categoryName);
+            //command.ExecuteNonQuery();
+            //connection.Close();
 
-            Console.WriteLine("Kategori Başarı ile Eklendi!");
-            
-            
-            
+            //Console.WriteLine("Kategori Başarı ile Eklendi!");
+            #endregion
+            #region Ürün ekleme işlemi
+            //string ProductName;
+            //decimal ProductPrice;
+            //bool ProductStatus;
+
+            //Console.Write("Eklemek İstediğiniz Ürünün ADı: ");
+            //ProductName = Console.ReadLine();
+            //Console.Write("Eklemek İstediğiniz Ürünün Fiyatı: ");
+            //ProductPrice = decimal.Parse(Console.ReadLine());
+
+            //SqlConnection conn = new SqlConnection("Data Source = DESKTOP-OBIK10F;initial catalog = EgitimKampidb;integrated security = true ");
+            //conn.Open();
+            //SqlCommand comm = new SqlCommand("insert into Table_2(ProductName,ProductPrice,ProductStatus) values (@p1,@p2,@p3)", conn);
+            //comm.Parameters.AddWithValue("@p1", ProductName);
+            //comm.Parameters.AddWithValue("@p2", ProductPrice);
+            //comm.Parameters.AddWithValue("@p3", true);
+            //comm.ExecuteNonQuery();
+            //conn.Close();
+            //Console.WriteLine("Ürün Başarı ile Eklendi!");
+            #endregion
+            #region Ürün listeleme işlemi
+
+            //SqlConnection conn = new SqlConnection("Data Source = DESKTOP-OBIK10F;initial catalog = EgitimKampidb;integrated security = true ");
+            //conn.Open();
+            //SqlCommand comm = new SqlCommand("select * from Table_2", conn);
+            //SqlDataAdapter adp = new SqlDataAdapter(comm);
+            //DataTable dT = new DataTable();
+            //adp.Fill(dT);
+            //foreach (DataRow row in dT.Rows)
+            //{
+            //    foreach(var item in row.ItemArray)
+            //    {
+            //        Console.Write(item.ToString() + " ");
+            //    }
+            //    Console.WriteLine(" ");
+            //}
+
+            //conn.Close();
+
+
+            #endregion
+            #region ÜrünSilme İşlemi
+
+            //Console.Write("Silmek istediğiniz ürünün Id'sini giriniz: ");
+            //int productId = int.Parse(Console.ReadLine());
+
+            //SqlConnection conn = new SqlConnection("Data Source = DESKTOP-OBIK10F;initial catalog = EgitimKampidb;integrated security = true ");
+            //conn.Open();
+
+            //SqlCommand comn = new SqlCommand("Delete from Table_2 Where ProductId = @productId",conn);
+            //comn.Parameters.AddWithValue("@productId", productId);
+            //comn.ExecuteNonQuery();
+
+            //conn.Close();
+
+            //Console.WriteLine("Ürün Başarı ile Silindi!");
+
+            #endregion
+            #region Ürün Güncelleme İşlemi
+
+            //Console.WriteLine("Güncellemek istediğiniz ürünün Id giriniz.");
+            //int productId = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Güncellemek istediğiniz ürünün adını giriniz.");
+            //string productName = Console.ReadLine();    
+            //Console.WriteLine("Güncellemek istediğiniz ürünün fiyatını giriniz.");
+            //decimal productPrice = decimal.Parse(Console.ReadLine());
+
+
+            //SqlConnection conn = new SqlConnection("Data Source = DESKTOP-OBIK10F;initial catalog = EgitimKampidb;integrated security = true ");
+            //conn.Open();
+
+            //SqlCommand comn = new SqlCommand("Update Table_2 set ProductName = @productName ,ProductPrice = @productPrice where ProductId = @productId",conn);
+            //comn.Parameters.AddWithValue("@productId", productId);
+            //comn.Parameters.AddWithValue("@productName", productName);
+            //comn.Parameters.AddWithValue("@productPrice", productPrice);
+            //comn.ExecuteNonQuery();
+
+
+            //conn.Close();
+
+            //Console.WriteLine("Ürün Başarı ile Güncellendi!");
+
+            #endregion
+
             Console.Read();
 
         }
